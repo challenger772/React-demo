d601825c78b07bffea5cfa41952baa28fa9e20eb
challenger772 <chalenger772@outlook.com>
2020-04-06 03:24:02 +0800

    second

--- src/components/multiple/index.js
+++ src/components/multiple/index.js
@@ -5,13 +5,45 @@ class Multiple extends PureComponent {
         super(props)
 
         this.state = {
-            
+            isGoing: true,
+            numberOfGuests: 2
         }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e) {
+        const target = e.target.value;
+        const value = target.value === this.isGoing ? target.checked : target.value;
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
     }
 
     render() {
         return (
-            
+            <form>
+                <label>
+                    isGoing:
+                    <input 
+                        name='isGoing'
+                        type='checkbox'
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    number of guests:
+                    <input 
+                        name='numberOfGuests'
+                        type='number'
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+            </form>
         )
     }
 }
