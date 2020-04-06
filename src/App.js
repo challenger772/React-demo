1fae60f101d0bccb410a87559e0ce356b7be883e
challenger772 <chalenger772@outlook.com>
2020-04-06 11:41:25 +0800

    composition

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Form from './components/forms/index';
 import Multiple from './components/multiple/index';
 import Calculator from './components/bolling-vertict/index'
 import Calculator1 from './components/temperature'
+import WelcomeDialog from './components/composition'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form />
-        <Multiple />
-        <Calculator />
-        <Calculator1 />
+        <Form /><br/>
+        <Multiple /><br/>
+        <Calculator /><br/>
+        <Calculator1 /><br/>
+        <WelcomeDialog /><br/>
       </div>
     )
   }
